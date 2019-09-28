abdb11f3c71fe20c80b8f2c13f2da1908fd6d878
FedExPipah <55308518+FedExPipah@users.noreply.github.com>
2019-09-28 16:24:56 -0700

    Create clicky-lawnmower.js

---
+++ clicky-lawnmower.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(500, 400);
+  colorMode(HSB);
+}
+
+var x = 0;
+var h = 15;// original height 
+
+function draw() {
+  stroke(random(60, 70), 100, 90);
+  line(x, height-10, x+random(10, 50), height-10-random(h));
+ noStroke(); // if has stroke then it will have a little green squar on the background
+
+  x = x + 10; // the spot of how whide the grass will grow
+
+  if (x > width) {
+    x = random(10);
+    h = h + 5; // continue growing bc the if statement is T 
+  }
+
+  if (mouseIsPressed) {
+    //(random(100) > 99.9) { 
+    fill(255);
+   rect(0, 0, width, height-15); //the lawnmower 
+  //  h = 8; // will come at grass h=10
+  }
+  
+
+  fill(40, 100, 60);
+  rect(0, height-10, width, 10); // ground 
+}
