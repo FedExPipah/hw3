3c1fd2519d04cb2b7a757cc2fc50fa8afa5a23fa
FedExPipah <55308518+FedExPipah@users.noreply.github.com>
2019-09-28 16:27:34 -0700

    Update clicky-lawnmower.js

--- clicky-lawnmower.js
+++ clicky-lawnmower.js
@@ -4,14 +4,14 @@ function setup() {
 }
 
 var x = 0;
-var h = 15;// original height 
+var h = 15; // original height 
 
 function draw() {
   stroke(random(60, 70), 100, 90);
-  line(x, height-10, x+random(10, 50), height-10-random(h));
- noStroke(); // if has stroke then it will have a little green squar on the background
+  line(x, height - 10, x + random(-10, 10), height - 10 - random(h));
+  noStroke(); // if has stroke then it will have a little green squar on the background
 
-  x = x + 10; // the spot of how whide the grass will grow
+  x = x + 50; // the spot of how whide the grass will grow
 
   if (x > width) {
     x = random(10);
@@ -21,11 +21,11 @@ function draw() {
   if (mouseIsPressed) {
     //(random(100) > 99.9) { 
     fill(255);
-   rect(0, 0, width, height-15); //the lawnmower 
-  //  h = 8; // will come at grass h=10
+    rect(0, 0, width, height - 15); //the lawnmower 
+    h = 8; // will come at grass h=10
   }
-  
+
 
   fill(40, 100, 60);
-  rect(0, height-10, width, 10); // ground 
+  rect(0, height - 10, width, 10); // ground 
 }
